bdd99dd33c2e6d32d7acf06103a3ec44343bbf01
xu0w0xu <xu0w0xu@163.com>
2023-09-04 01:23:47 +0800

    完成题目

---
+++ leetcode/319.灯泡开关.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <cmath>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=319 lang=cpp
+ *
+ * [319] 灯泡开关
+ */
+
+ // @lc code=start
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return (int)sqrt(n);
+    }
+};
+// @lc code=end
+
